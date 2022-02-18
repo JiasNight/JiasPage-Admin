98a97341bc23fcd19ba742686d94d41e27d34614
JiasNight <Jias96@163.com>
2022-02-18 17:43:16 +0800

    初始化创建项目

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Index",
+    component: () => import('@/views/index.vue'),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import('@/views/login/index.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="view-account">
+    登录
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from 'vue';
+</script>
+
+<style lang="scss" scoped>
+</style>

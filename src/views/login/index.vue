27849fd5d3a3290b701a095b348870053a6ee69f
JiasNight <Jias96@163.com>
2022-08-22 19:00:22 +0800

    登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,40 +5,48 @@
       <div class="box-right">
         <div class="right-login">
           <p class="login-title">标题</p>
-          <div class="login-form"></div>
+          <div class="login-form">
+            <v-container>
+              <v-form>
+                <v-text-field
+                  v-model="adminForm.userName"
+                  required
+                  :rules="adminFormRules.userName"
+                  :counter="10"
+                  label="登录账户"
+                  prepend-inner-icon="mdi-account-lock"
+                  variant="outlined"
+                  color="success"
+                  density="compact"
+                  shaped
+                >
+                </v-text-field>
+                <v-text-field
+                  v-model="adminForm.password"
+                  required
+                  :rules="adminFormRules.password"
+                  :counter="10"
+                  label="登录密码"
+                  prepend-inner-icon="mdi-account-key"
+                  variant="outlined"
+                  color="success"
+                  density="compact"
+                  shaped
+                ></v-text-field>
+                <v-divider></v-divider>
+                <div class="form-tool">
+                  <span>
+                    <v-checkbox label="记住密码" color="orange" value="success" hide-details></v-checkbox>
+                  </span>
+                  <span>忘记密码？</span>
+                </div>
+                <v-btn rounded="pill" color="primary" block> <span>登 录</span></v-btn>
+              </v-form>
+            </v-container>
+          </div>
         </div>
       </div>
     </div>
-    <!-- <v-container>
-      <v-form>
-        <v-text-field
-          v-model="adminForm.userName"
-          required
-          :rules="adminFormRules.userName"
-          :counter="10"
-          label="登录账户"
-          prepend-inner-icon="mdi-account-lock"
-          variant="outlined"
-          color="success"
-          density="compact"
-          shaped
-        >
-        </v-text-field>
-
-        <v-text-field
-          v-model="adminForm.password"
-          required
-          :rules="adminFormRules.password"
-          :counter="10"
-          label="登录密码"
-          prepend-inner-icon="mdi-account-key"
-          variant="outlined"
-          color="success"
-          density="compact"
-          shaped
-        ></v-text-field>
-      </v-form>
-    </v-container> -->
   </div>
 </template>
 
@@ -69,7 +77,7 @@ const adminFormRules = reactive({
   .content-box {
     width: 1000px;
     height: 550px;
-    background-color: aquamarine;
+    background-color: rgb(50, 132, 199);
     box-shadow: 10px 10px 10px 0 rgb(158, 153, 153);
     position: absolute;
     left: 50%;
@@ -93,6 +101,17 @@ const adminFormRules = reactive({
           font-weight: 600;
           text-align: center;
           padding: 20px;
+        }
+        .login-form {
+          width: 75%;
+          margin: 0 auto;
+          margin-top: 30px;
+          .form-tool {
+            display: flex;
+            flex-direction: row;
+            align-content: space-between;
+            justify-items: center;
+          }
         }
       }
     }

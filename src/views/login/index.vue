ea6eb9bce16f5741223410ce644e5f2ef7329750
jias <Jias96@163.comm>
2022-08-22 23:19:12 +0800

    登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="login-container">
     <div class="content-box">
-      <div class="box-left"></div>
+      <div v-ripple class="box-left text-center elevation-2 pa-12 text-h5" >
+        <img src="https://picsum.photos/id/11/500/300" alt="">
+        <br>
+        <br>
+        <p>
+          如果你拥有别人没有的，知道别人不知道的，会做别人不会做的，请准备好：别人都会来批评你。如果想杜绝批评，那你不妨做一个无脑无能无言无为的人
+        </p>
+      </div>
       <div class="box-right">
         <div class="right-login">
           <p class="login-title">标题</p>
@@ -17,12 +24,14 @@
                   prepend-inner-icon="mdi-account-lock"
                   variant="outlined"
                   color="success"
-                  density="compact"
+                  density="compact" 
                   shaped
                 >
                 </v-text-field>
                 <v-text-field
                   v-model="adminForm.password"
+                  :type="showPassword ? 'text' : 'password'"
+                  :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
                   required
                   :rules="adminFormRules.password"
                   :counter="10"
@@ -32,13 +41,35 @@
                   color="success"
                   density="compact"
                   shaped
+                  @click:append-inner="showPassword = !showPassword"
                 ></v-text-field>
+                <div class="form-verify-code">
+                  <div class="code-input">
+                    <v-text-field
+                      v-model="adminForm.verifyCode"
+                      required
+                      :rules="adminFormRules.verifyCode"
+                      :counter="4"
+                      label="验证码"
+                      prepend-inner-icon="mdi-barcode-scan"
+                      variant="outlined"
+                      color="success"
+                      density="compact"
+                      shaped
+                    ></v-text-field>
+                  </div>
+                  <img class="code-look" src="https://picsum.photos/id/11/500/300" alt="verifyCode">
+                </div>
                 <v-divider></v-divider>
                 <div class="form-tool">
-                  <span>
-                    <v-checkbox label="记住密码" color="orange" value="success" hide-details></v-checkbox>
-                  </span>
-                  <span>忘记密码？</span>
+                  <v-checkbox
+                    class="tool-remember-password"
+                    label="记住密码"
+                    color="orange"
+                    value="success"
+                    hide-details>
+                  </v-checkbox>
+                  <a class="tool-forget-password" href="#">忘记密码？</a>
                 </div>
                 <v-btn rounded="pill" color="primary" block> <span>登 录</span></v-btn>
               </v-form>
@@ -58,40 +89,54 @@ const email = $ref('');
 
 const adminForm = reactive({
   userName: '',
-  password: ''
+  password: '',
+  verifyCode: ''
 });
 
 const adminFormRules = reactive({
   userName: [(v: any) => !!v || '请输入用户名！', (v: any) => v.length <= 3 || '用户名长度太短！'],
-  password: [(v: any) => !!v || '请输入密码！', (v: any) => /.+@.+/.test(v) || '密码过于简单了！']
+  password: [(v: any) => !!v || '请输入密码！', (v: any) => /.+@.+/.test(v) || '密码过于简单了！'],
+  verifyCode: [(v: any) => !!v || '请输入验证码！']
 });
+
+const showPassword = $ref(false);
 </script>
 
 <style lang="scss" scoped>
 .login-container {
   width: 100vw;
   height: 100%;
-  // background-image: url('/src/assets/images/admin/login/autumn.jpg');
+  background-image: url('/src/assets/images/admin/login/spring.jpg');
+  background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
   .content-box {
     width: 1000px;
     height: 550px;
-    background-color: rgb(50, 132, 199);
-    box-shadow: 10px 10px 10px 0 rgb(158, 153, 153);
+    background: linear-gradient(180deg,#5efce8,#736efe);
+    box-shadow: 10px 10px 10px 0 rgb(192, 192, 192);
     position: absolute;
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
     display: flex;
     flex-direction: row;
-    align-content: center;
-    justify-items: center;
-    border-radius: 5px;
+    justify-content: center;
+    border-radius: 10px;
     .box-left {
       width: 45%;
       height: 100%;
       border-right: 2px solid rgb(158, 150, 150);
+      img {
+        height: 50%;
+      }
+      p {
+        height: 50%;
+        text-indent: 2em;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: wrap;
+      }
     }
     .box-right {
       width: 55%;
@@ -100,17 +145,42 @@ const adminFormRules = reactive({
           font-size: 25px;
           font-weight: 600;
           text-align: center;
-          padding: 20px;
+          padding: 30px;
         }
         .login-form {
           width: 75%;
           margin: 0 auto;
-          margin-top: 30px;
+          .form-verify-code {
+            display: flex;
+            flex-direction: row;
+            align-items: flex-start;
+            justify-content: space-between;
+            .code-input {
+              width: 60%;
+            }
+            .code-look {
+              width: 35%;
+              height: 40px;
+              cursor: pointer;
+            }
+          }
           .form-tool {
             display: flex;
             flex-direction: row;
-            align-content: space-between;
-            justify-items: center;
+            align-items: center;
+            justify-content: space-around;
+            .tool-remember-password {
+              color: #2ecc71;
+            }
+            .tool-remember-password:hover {
+              color: #ffa502; 
+            }
+            .tool-forget-password {
+              color: #2ecc71;
+            }
+            .tool-forget-password:hover {
+              color: #ffa502; 
+            }
           }
         }
       }

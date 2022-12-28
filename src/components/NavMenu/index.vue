32d5d676709d88ebb712b2974b87cf110b8b858d
JiasNight <Jias96@163.com>
2022-12-28 20:55:21 +0800

    树形菜单

--- src/components/NavMenu/index.vue
+++ src/components/NavMenu/index.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="nav-content">
-    <div v-for="(menu, index) in currentProps.navMenus" :key="index" class="content-menu">
+  <div class="nav-wrapper">
+    <div v-for="(menu, index) in currentProps.navMenus" :key="index" class="wrapper-menu">
       <!-- 菜单 -->
       <div class="menu-item">
-        <div class="item-left">
+        <div class="item-left" :style="{ 'padding-left': (menu.level - 1) * 30 + 'px' }">
           <v-icon :icon="menu.icon"></v-icon>
-          <span class="item-name">{{ menu.name }}</span>
+          <span class="item-name" :title="menu.name">{{ menu.name }}</span>
         </div>
         <v-icon v-if="menu.children" icon="mdi-chevron-up"></v-icon>
       </div>
-      <!-- 目录 -->
-      <div v-if="menu.children" class="menu-subitem">
-        <NavMenu v-if="menu.children" :nav-menus="menu.children"></NavMenu>
-      </div>
+      <NavMenu v-if="menu.children" :nav-menus="menu.children"></NavMenu>
     </div>
   </div>
 </template>
@@ -25,6 +22,7 @@ interface IMenu {
   pid: string;
   type: number;
   name: string;
+  level: number;
   icon: string;
   path: string;
   show: boolean;
@@ -49,9 +47,10 @@ const currentProps = defineProps({
   }
 }
 
-.nav-content {
+.nav-wrapper {
   width: 100%;
-  .content-menu {
+  .wrapper-menu {
+    width: 100%;
     .menu-item {
       display: flex;
       flex-direction: row;
@@ -64,20 +63,25 @@ const currentProps = defineProps({
       .item-left {
         display: flex;
         flex-direction: row;
-        justify-content: space-between;
+        justify-content: start;
         align-self: center;
         margin-left: 10px;
+        width: 85%;
         .item-name {
+          overflow: hidden; //超出的文本隐藏
           margin-left: 10px;
+          width: 100%;
+          text-overflow: ellipsis; //溢出用省略号显示
+          white-space: nowrap; // 默认不换行；
         }
       }
       &:hover {
         background-color: #7f8c8d6e;
       }
     }
-    .menu-subitem {
-      padding-left: 30px;
-    }
+  }
+  .menu-subitem {
+    padding-left: 30px;
   }
 }
 </style>
